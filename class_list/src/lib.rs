e9fee910d533e6e52832904e49ae4d384ca9391d
Kyza <kyza@kyza.net>
2023-08-21 13:38:30 -0400

    docs: symlink README.md build: bump version

--- class_list/src/lib.rs
+++ class_list/src/lib.rs
@@ -1,17 +1,11 @@
-#![cfg(publish)]
 #![doc = include_str!("../README.md")]
-#![cfg(!publish)]
-#![doc = include_str!("../../README.md")]
 
 #[doc(hidden)]
 /// Not part of the public API.
 pub use class_list_macro::class_list as __class_list;
 
 #[macro_export]
-#[cfg(publish)]
 #[doc = include_str!("../README.md")]
-#[cfg(!publish)]
-#[doc = include_str!("../../README.md")]
 macro_rules! class_list {
 	( $($input:tt)* ) => ({
 		$crate::__class_list! {
